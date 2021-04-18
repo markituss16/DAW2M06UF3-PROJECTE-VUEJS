3d07d9da1d5ca38dc9f78269aae8116d4be1a019
sealja2021daw2 <sealja2021daw2@gmail.com>
2021-04-18 21:57:23 +0200

    animacions i transicions

--- components/inicial.js
+++ components/inicial.js
@@ -1,10 +1,15 @@
 var inicial = Vue.component("Inicial",{
+      data: function(){
+        return {
+          show: true
+        }
+      },
       template:`
         <div class="container">
-            <img style="margin-top: 40px; margin-left:340px" src="../assets/images/sopaLletres.png" class="img-fluid" width="400">
-            <h1 style="margin-top: 20px; text-align: center;">SOPA DE LLETRES</h1>
-            <p style="margin-top: 40px; text-align: center;"><strong>Sergio Aliaga i Marc Palma</strong></p>
-        </div>
+        <img  style="margin-top: 40px; margin-left:340px" class="animated bounce infinite img-fluid" src="../assets/images/sopaLletres.png" width="400">
+        <h1 style="margin-top: 20px; text-align: center; color:white">SOPA DE LLETRES</h1>
+        <p style="margin-top: 40px; text-align: center; color:white"><strong>Sergio Aliaga i Marc Palma</strong></p>
+        </div>        
       `
 });
 
--- components/wordList.js
+++ components/wordList.js
@@ -1,5 +1,10 @@
 var wordList = Vue.component("WordList",{
-    name: 'WordList',
+	data: function(){
+        return {
+          show: true
+        }
+      },
+	name: 'WordList',
 	props:{
 		palabras:{
 			type: Array,
@@ -8,13 +13,18 @@ var wordList = Vue.component("WordList",{
 	},
     template: `
     <div class="col-12 col-md-3 col-lg-5 col-xl-7 pt-4 pb-5">
-	    <div class="list-pals p-1 bg-smoke rounded-0 row m-0">
-		    <div v-for="(pl,pli) in palabras" 
+	<transition name="bounce">
+	    <div v-if="show" class="list-pals p-1 bg-smoke rounded-0 row m-0">
+			<div  v-for="(pl,pli) in palabras" 
 			    class="p-2 col-auto palabra boogaloo text-uppercase text-center rounded py-3 px-4" 
 			    :class="{'encontrada':pl.e}" :style="{'background-color': (pl.e?pl.h:'') }">
 			    {{pl.w}}
 		    </div>
 	    </div>
+	</transition>
+		<button @click="show = !show">
+		MOSTRAR PARAULES
+	  	</button>
     </div>
     `
 });
